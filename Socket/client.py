ff9f744b8c5ce955f346503e3575bc5c96c4136b
rawrrawrpurpledinosaur <191292L@students.hci.edu.sg>
2024-05-19 18:02:11 +0800

    socket template 2

--- Socket/client.py
+++ Socket/client.py
@@ -4,4 +4,16 @@ sock = socket.socket()
 
 sock.connect(('localhost', 5000))
 
+end = False
+
+while not end: 
+    message = input("Enter a message: ")
+    sock.send(message.encode())
+    if message == "exit":
+        end = True
+    else:
+        response = sock.recv(1024)
+        print(response.decode())
+
+sock.close()
 
--- Socket/server.py
+++ Socket/server.py
@@ -7,3 +7,16 @@ sock.bind(('localhost', 500))
 sock.listen()
 
 conn, addr = sock.accept()
+
+end = False
+
+while not end: 
+    data = conn.recv(1024)
+    if not data:
+        end = True
+    else:
+        print(data.decode())
+        conn.send(data)
+
+conn.close()
+sock.close()
